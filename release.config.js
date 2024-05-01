9537248b6665f5c3683967547813cf3556b5f7b3
Dinesh Sodhi <dineshsodhi@Dineshs-MacBook-Air.local>
2024-05-01 16:36:46 -0700

    added new code

--- release.config.js
+++ release.config.js
@@ -1,11 +1,14 @@
-module.exports = {
+const config = {
     branches: ['main'],
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      '@semantic-release/github',
-      '@semantic-release/git',
-    ],
+      ["@semantic-release/git", {
+        "assets": ["dist/*.js", "dist/*.js.map"],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }],
+      '@semantic-release/github'
+    ]
   };
   
+  module.exports = config;
